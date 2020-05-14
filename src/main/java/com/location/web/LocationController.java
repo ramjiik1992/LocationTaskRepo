dd15241b122d3454baa84dfba5416f8f94b9303d
ramji <ramjik1992@gmail.com>
2020-05-14 17:39:22 +0530

    controller changes

--- src/main/java/com/location/web/LocationController.java
+++ src/main/java/com/location/web/LocationController.java
@@ -1,18 +1,25 @@
 package com.location.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.location.domain.Location;
+import com.location.service.LocationService;
 
 @RestController("api/v1")
 public class LocationController {
+  
+  @Autowired
+  private LocationService service;
 
   
   @PostMapping("/location")
-  public ResponseEntity<Location> createLocationDetails(@RequestBody Location  locationData){
-    return null;
+  public ResponseEntity<Location> createLocationDetails(@RequestBody Location  location){
+    
+    return ResponseEntity.status(HttpStatus.CREATED).body(service.createLocation(location));
     
   }
 }
